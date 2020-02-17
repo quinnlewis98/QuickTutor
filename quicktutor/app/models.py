aca3729ad5c8e7bd1131879187c91be4c9f47d5a
Sean Burtner <seb4jw@virginia.edu>
2020-02-16 20:21:47 -0500

    adding some empty models

--- quicktutor/app/models.py
+++ quicktutor/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+#from django.contrib.auth import models as other_models <--- django has a built in User model that we may want to use
 
 # Create your models here.
+# we will need these models at least
+
+
+class User(models.Model):
+    pass
+
+
+class Request(models.Model):
+    pass
+
+
+class Conversation(models.Model):
+    pass
+

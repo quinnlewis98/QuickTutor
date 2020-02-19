cc95dd71f62dbd1a795fe8cf7728184f4f7d22bd
Sean Burtner <seb4jw@virginia.edu>
2020-02-19 14:16:24 -0500

    redirect to redirect page after sign in

--- app/views.py
+++ app/views.py
@@ -6,6 +6,9 @@ from .models import *
 def index(request):
     return render(request, 'app/index.html')
 
+def redirect(request):
+    return render(request, 'app/redirect.html')
+
 def feed(request):
     requests_list = Request.objects.order_by('-pub_date')[:]
     # process each request in a for loop

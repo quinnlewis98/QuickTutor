b0727e09d470cdef2cd5d1ebeb86456d9c5986a0
Sean Burtner <seb4jw@virginia.edu>
2020-02-19 14:47:42 -0500

    create myRequest page

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('redirect/', views.redirect, name='redirect'),
     path('feed/', views.feed, name='feed'),
+    path('myRequest/', views.myRequest, name='myRequest'),
 ]

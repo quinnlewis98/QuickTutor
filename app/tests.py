7277198653fa260d46d468714af2aa7db462a457
jz5jx <johnzhang@d-172-25-203-187.dhcp.virginia.edu>
2020-02-26 15:53:13 -0500

    NewTest

--- app/tests.py
+++ app/tests.py
@@ -1,7 +1,14 @@
 from django.test import TestCase
 import unittest
+from django.test import Client 
 
 class MyTestCase(unittest.TestCase):
+	def setUp(self):
+		c = Client()
+		response = c.post('/myRequest/',{'title': '1', 'location' : '2', 'description' : '3', 'pub_date' : '4', 'user' : '5'})
+		self.assertEqual(response.status_code,302)
+
+
 	def test_list_int(self):
   		data = [1, 2, 3]
   		result = sum(data)
--- quicktutor/test.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-import unittest
-
-class MyTestCase(unittest.TestCase):
-	def test_list_int(self):
-  		data = [1, 2, 3]
-  		result = sum(data)
-  		self.assertEqual(result, 6)

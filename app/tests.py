06d879f712a70a4fe79a83fc234f3a7ce021d64e
jz5jx <johnzhang@d-172-25-107-90.dhcp.virginia.edu>
2020-02-27 16:12:35 -0500

    tests

--- app/tests.py
+++ app/tests.py
@@ -6,10 +6,26 @@ class MyTestCase(unittest.TestCase):
 	def setUp(self):
 		c = Client()
 		response = c.post('/myRequest/',{'title': '1', 'location' : '2', 'description' : '3', 'pub_date' : '4', 'user' : '5'})
-		self.assertEqual(response.status_code,302)
+		self.assertEqual(response.status_code , 302)
 
+	def test_Feed(self):
+		a = Client()
+		response_feed = a.post('feed/',{})
+		self.assertEqual(response_feed.status_code , 404)
+		#Because there is no get Functionality yet, this should return a Not found page. 
+
+	def test_length_of_feed(self):
+		b = Client()
+		response_redirect = b.get('profile/', follow = True)
+		self.assertIsNotNone(response_redirect.redirect_chain)
 
 	def test_list_int(self):
   		data = [1, 2, 3]
   		result = sum(data)
   		self.assertEqual(result, 6)
+
+
+
+
+  		
+		

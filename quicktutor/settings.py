a236126e27b809e0129336cd900956243dc0ecad
Tyra Krehbiel <43380449+tyrakrehbiel@users.noreply.github.com>
2020-03-27 12:26:24 -0400

    Changes made to user profile model. Allows for user to upload profile pic

--- quicktutor/settings.py
+++ quicktutor/settings.py
@@ -129,6 +129,9 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 

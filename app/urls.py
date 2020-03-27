5d28336728217b8ec9b6af8e24d42c17406d318f
Tyra Krehbiel <43380449+tyrakrehbiel@users.noreply.github.com>
2020-03-27 12:46:43 -0400

    I am getting errors with google authentication at this point and am unable to log in to the app completely. This is commiting changes made before attempting to solve this situation

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+# needed for profile images
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -10,3 +13,6 @@ urlpatterns = [
     path('contacts/', views.contacts, name='contacts'),
     path('messages/', views.messages, name='messages'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
